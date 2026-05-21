6d6ae256ec6c322c69e8ffa81e421754315faf5f
Jeongeun Baek <127070509+bjo5029@users.noreply.github.com>
2026-05-21 15:43:39 +0900

    Add files via upload

--- code/3-2.c
+++ code/3-2.c
@@ -24,14 +24,22 @@ int cmp_desc(const void *a, const void *b) {
 }
 
 int main(void) {
-  int arr[] = {5, 2, 8, 1, 9, 3};
+  int arr_asc[] = {5, 2, 8, 1, 9, 3};
+  int arr_desc[] = {5, 2, 8, 1, 9, 3};
   int size = 6;
   int i;
 
-  qsort(arr, size, sizeof(int), cmp_asc);
+  qsort(arr_asc, size, sizeof(int), cmp_asc);
 
   for (i = 0; i < size; i++) {
-    printf("%d ", arr[i]);
+    printf("%d ", arr_asc[i]);
+  }
+  printf("\n");
+
+  qsort(arr_desc, size, sizeof(int), cmp_desc);
+
+  for (i = 0; i < size; i++) {
+    printf("%d ", arr_desc[i]);
   }
   printf("\n");
 
